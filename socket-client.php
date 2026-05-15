9bdb7a2f3b0ff78da9790e601538c267a5408168
Lucas Ferro <lucasferrobrandao@gmail.com>
2026-05-15 18:30:15 -0300

    feat: melhora comunicação enre client e server

--- socket-client.php
+++ socket-client.php
@@ -1,16 +1,60 @@
 <?php
 
+require 'socket-constants.php';
+
+$operationSystem = PHP_OS_FAMILY;
+
+if ($operationSystem == 'Windows') {
+    exec('echo %USERNAME%', $output);
+    $username = $output[0] ?? 'unknown';
+    unset($output);
+    exec('hostname', $output);
+    $hostname = $output[0] ?? 'unknown';
+} else {
+    exec('whoami', $output);
+    $username = $output[0] ?? 'unknown';
+    unset($output);
+    exec('hostname', $output);
+    $hostname = $output[0] ?? 'unknown';
+}
+
+echo "Coletou dados do cliente: {$hostname} - {$username} - {$operationSystem}\n";
+
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 
 socket_connect($socket, '127.0.0.1', 4000);
 
+echo "Conectado ao socket server\n";
+
 while(true) {
     $response = socket_read($socket, 1024);
 
-    if($response === false) {
-        echo "Server has disconnected\n";
+    if ($response === false) {
+        $socketCode = socket_last_error($socket);
+
+        if (in_array($socketCode, [SOCKET_ERROR_NON_BLOCKING_OPERATION, SOCKET_SUCCESS_CONNECTION])) {
+            continue;
+        }
+
+        echo socket_strerror($socketCode);
+
         break;
     }
 
-    echo $response;
+    if ($response == 'GET_INFO_CLIENT') {
+        $info = json_encode([
+            'hostname' => $hostname,
+            'username' => $username,
+            'operationSystem' => $operationSystem
+        ]);
+        
+        $result = socket_write($socket, "GET_INFO_CLIENT:{$info}");
+
+        echo "Enviou dados do cliente para o servidor: {$result}\n";
+    }
+
+    if (strpos($response, 'DATA_EVENT:') !== false) {
+
+        echo "Recebeu evento: {$response}\n";
+    }
 }
--- socket-server.php
+++ socket-server.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'socket-constants.php';
+
 $clients = array();
 $messages = array();
 
@@ -14,51 +16,100 @@ socket_set_nonblock($socket);
 echo "Server is running on port 4000\n";
 
 while(true) {
-    if(($newSocket = socket_accept($socket)) !== false) {
+    if (($newSocket = socket_accept($socket)) !== false) {
         var_dump($newSocket);
+
+        socket_getpeername($newSocket, $ip, $port);
         
-        echo "Client has connected\n";
+        echo "Client has connected: {$ip}:{$port}\n";
         
-        $clients[] = $newSocket;
+        $clients["{$ip}:{$port}"] = [
+            'socket' => $newSocket, 
+            'ip' => $ip, 
+            'port' => $port
+        ];
+
+        echo "Client added to clients array\n";
+
+        $result = socket_write($newSocket, "GET_INFO_CLIENT");
+
+        echo "Solicitou informações do cliente: {$result}\n";
+
+        var_dump("Clientes:\n");
+        var_dump($clients);
     }
 
-    foreach($clients as $key => $client) {
-        $data = socket_read($client, 1024);
+    foreach($clients as $key => &$client) {
+        $data = socket_read($client['socket'], 1024);
 
-        var_dump("data:");
-        var_dump($data);
-        echo PHP_EOL;
-        
-        if($data === false) {
-            // echo "Client $key has disconnected\n";
-            
-            // unset($clients[$key]);
-            
+        if ($data === false) {
+            $socketCode = socket_last_error($socket);
+
+            var_dump($socketCode);
+            var_dump(socket_strerror($socketCode));
+
+            if (! in_array($socketCode, [SOCKET_ERROR_NON_BLOCKING_OPERATION, SOCKET_SUCCESS_CONNECTION])) {
+                $clientIp = $client['ip'];
+                $clientPort = $client['port'];
+
+                echo "Client [$key] {$clientIp}:{$clientPort}\n";
+                echo socket_strerror($socketCode) . PHP_EOL;
+
+                unset($clients[$key]);
+            }
+
             continue;
         }
 
-        $dados = explode(PHP_EOL, $data);
+        var_dump("Leu dados do cliente: {$data}\n");
+        
+        if (strpos($data, 'GET_INFO_CLIENT:') !== false) {
+            $dataModified = str_replace('GET_INFO_CLIENT:', '', $data);
+            $clientInfo = json_decode($dataModified, true);
 
+            $client += $clientInfo;
 
-        $requestMethod = explode(' ', $dados[0])[0];
-        $requestUri = explode(' ', $dados[0])[1];
-        $requestBody = json_decode($dados[6], true);
+            echo "Recebeu informações do cliente: {$data}\n";
 
-        if ($requestMethod == 'POST' && $requestUri == '/api/v1/events') {
-            
+            continue;
+        }
+
+        if (strpos($data, 'POST /api/v1/events HTTP') !== false) {
+            $dados = explode(PHP_EOL, $data);
+
+            $requestBody = json_decode($dados[6], true);
+
             $message = json_encode([
                 'event' => $requestBody['event'], 
                 'channel' => $requestBody['channel']
             ]);
-            
+
+            $message = "DATA_EVENT:{$message}";
+
             foreach($clients as $key2 => $client2) {
 
+                $text = <<<TEXT
+                    HTTP/1.1 200 OK
+                    Content-Type: application/json
+                    Content-Length: 40
+
+                    {"message":"Evento enviado com sucesso"}
+TEXT;
+
                 if ($key2 == $key) {
+                    $result = socket_write(
+                        $client2['socket'], 
+                        $text
+                    );
+
+                    socket_close($client2['socket']);
+                    unset($clients[$key2]);
+
                     continue;
                 }
 
                 $result = socket_write(
-                    $client2, 
+                    $client2['socket'], 
                     $message
                 );
     
@@ -68,6 +119,5 @@ while(true) {
             }
         }
 
-        exit;
     }
 }
